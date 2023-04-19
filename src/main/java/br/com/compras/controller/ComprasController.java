f84b12cba92c3f9f70f310b0f3b73354abebd849
DavidPaulinus <davipm@hotmail.com>
2023-04-19 08:18:58 -0300

    Controller criado

---
+++ src/main/java/br/com/compras/controller/ComprasController.java
@@ -0,0 +1,10 @@
+package br.com.compras.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/compras")
+public class ComprasController {
+	
+}
